89d4c3f02c3e4aa1d41c2547ee5c0e5731f43b35
W. Logan Downing <downingstech@gmail.com>
2018-10-30 16:52:54 -0500

    Data Ingestion & Object Creation Loop
    Prepared the Data ingestion and object creation loop for Qicheng to retrofit Keyuan's objects.
    
    Updated GitIgnore so that real data can be kept in personal repositories.

--- dataStreaming2.py
+++ dataStreaming2.py
@@ -7,15 +7,17 @@ df = pd.read_csv(PATH_LOAD)
 
 sorted_df = df.sort_values(by=['Global_Time'])
 
+#initialized as the first time in the sorted dataframe
+time = sorted_df['Global_Time'][0] # UNIX time based
 
-time = '49:39.7'
+# sorted_df[sorted_df['Global_Time'] == time]
 
-sorted_df.Vehicle_ID.dtype
+# below can be used to generate objects
 
-'''
 for i in range(0, len(sorted_df)):
-    print(sorted_df.iloc[i, :])
-'''
-
-sorted_df["Global_Time"][0]
-
+    timeStep_df = sorted_df[sorted_df['Global_Time'] == time]
+    if(len(timeStep_df) != 0): # loop through the time step dataframe
+        time += 100 # update the time step on each iteration
+        print(timeStep_df)
+    else:
+        break
--- main.py
+++ main.py
@@ -53,19 +53,36 @@ while True:
     render.Background(screen).draw()  # render the screen
 
     # generate and draw all car objects
-    
+
+    for i in range(0, len(sorted_df)):
+    timeStep_df = sorted_df[sorted_df['Global_Time'] == time]
+    if(len(timeStep_df) != 0): # loop through the time step dataframe
+        time += 100 # update the time step on each iteration
+        #print(timeStep_df)
+
+        #THIS IS WHERE THE OBJECTS WILL BE GENERATED FROM KEYUAN'S OBJECT FILE
+    else:
+        break
 
     '''Original car rendering from csv
     for key in carData.keys():
         render.Car(screen, cid=key, direction=carData[key][0], speed=carData[key][1],
                    x=carData[key][2], y=carData[key][3]).draw()
-        '''
+    '''
+
     # update car x-position
+
+
+    
+    ''' THIS WAS THE ORIGINAL CARDATA POSITION UPDATE FOR GUI PURPOSES
     for key in carData.keys():
         if carData[key][2] <= Size[0]:
             carData[key][2] += carData[key][1]/5  # speed cut by factor of 5 to prevent cars from looking too fast
         else:
             carData[key][2] = -20
+    '''
+    
+    #NOTE: THIS SECTION NEEDS UPDATED ONCE KEYUAN'S OBJECTS ARE CREATED
 
     pygame.display.update()
     clock.tick(fps)
